62bc22967fad889382b2a94ede1afe1cbc1c7290
Jesus <berriojesus122@gmail.com>
2021-05-13 15:37:20 -0500

    what can i do done

--- src/frontend/components/aboutMe.jsx
+++ src/frontend/components/aboutMe.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 const moment = require('moment')
-moment("20201210", "YYYYMMDD").fromNow()
 import { SiJavascript, DiReact, AiFillGithub, SiHtml5, SiCss3, SiSass, AiFillApi, SiWebpack, GrNode, HiServer, SiMongodb, GiSecurityGate, DiJavascript1, SiNpm, SiRedux } from 'react-icons/all'
 import './styles/aboutMe.scss'
 function aboutMe() {
+    const given = moment("2020-12-10", "YYYY-MM-DD");
+    const current = moment().startOf('day')
     return (
         <article className='about'>
             <h2 className='about-title'>{'<About me>'}</h2>
             <section className='about-me'>
                 <h4><b>+600</b><br/> hours experience</h4>
-                <h4><b>{moment("20201210", "YYYYMMDD").fromNow()}</b> <br/> i started</h4>
+                <h4><b>{moment.duration(given.diff(current)).asDays() * -1}</b> <br/>days since i start</h4>
                 <h4><b>137</b><br/>commits</h4>
                 <h4><b>41</b> <br/> programming courses pass</h4>
             </section>
@@ -34,9 +35,9 @@ function aboutMe() {
             </ul>
             </section>
 
-            <section>
-            <h3>What can i do?</h3>
-            <ul className='about-skills'>
+            <section className='about-skills'>
+            <h3 className='about-skills-title'>What can i do?</h3>
+            <ul className='about-skills-list'>
                 <li>Responsive design</li>
                 <li>Progressive web apps</li>
                 <li>Server side rendering</li>
