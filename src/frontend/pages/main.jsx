a3784efd73b632607376372cffc08acbe02c880d
Jesus <berriojesus122@gmail.com>
2021-05-13 13:02:07 -0500

    proects done

--- src/frontend/pages/main.jsx
+++ src/frontend/pages/main.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import Header from '../components/header'
 import Hero from '../components/hero'
 import Projects from '../components/projects'
+import AboutMe from '../components/aboutMe'
 function main() {
     return (
         <>
             <Header />
             <Hero />
+            <AboutMe />
             <Projects />
         </>
     )
