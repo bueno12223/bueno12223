e6b1ee7204843a466c3956ecca1a43cffde6a8bc
Jesus <berriojesus122@gmail.com>
2021-05-13 10:36:50 -0500

    proects done

---
+++ src/frontend/components/projects.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './styles/projects.scss'
+function projects() {
+    return (
+        <article className='projects'>
+            <h2 className='projects-title'>{'<Projects>'}</h2>
+            <div className='projects-card-container'>
+            <section className="projects-card"></section>
+            <section className="projects-card"></section>
+            <section className="projects-card"></section>
+            <section className="projects-card"></section>
+            </div>
+
+        </article>
+    )
+}
+
+export default projects
--- src/frontend/pages/main.jsx
+++ src/frontend/pages/main.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from '../components/header'
 import Hero from '../components/hero'
+import Projects from '../components/projects'
 function main() {
     return (
         <>
             <Header />
             <Hero />
+            <Projects />
         </>
     )
 }
