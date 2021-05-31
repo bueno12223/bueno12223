7b1ce7e4dc3023fe4b96ca9349e31f333a7051df
Jesus Berrio <berriojesus122@gmail.com>
2021-05-31 14:20:34 -0400

    nuevos frameworks y colores

---
+++ src/frontend/assets/icons/graphql.jsx
@@ -0,0 +1,22 @@
+import * as React from "react"
+
+function SvgComponent() {
+  return (
+    <svg
+      stroke="currentColor"
+      fill="currentColor"
+      strokeWidth={0}
+      viewBox="0 0 24 24"
+      height="1em"
+      width="1em"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M14.3 2.9L19 5.6l.5-.4A2.2 2.2 0 1121 9.3v5.4a2.2 2.2 0 11-2.1 3.5L14.2 21a2.2 2.2 0 11-4.1.2l-4.7-2.7-.6.4a2.2 2.2 0 11-1.7-4V9.2A2.2 2.2 0 011.8 6a2.2 2.2 0 013.5-.5L10 2.7v-.5a2.2 2.2 0 114.3.7zm-.5.8l-.1.2L20 14.8V9.2a2.2 2.2 0 01-1.5-2.9l-4.7-2.7zm-3.2 0h-.1L5.8 6.2a2.2 2.2 0 01-1.7 3v5.4h.1l6.4-11zm2.2.7a2.2 2.2 0 01-1.4 0L5.1 15.1a2.2 2.2 0 01.8 1h12.6a2.2 2.2 0 01.6-1L12.8 4.5zm1 15.8l4.7-2.8v-.1H5.9v.2l4.7 2.7a2.2 2.2 0 013.1 0z"
+        stroke="none"
+      />
+    </svg>
+  )
+}
+
+export default SvgComponent
